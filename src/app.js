dd8279e9a00e1ce007c1d17680ad876c52d0a08b
Abdulfatai <fob90s@gmail.com>
2022-01-04 07:54:46 +0100

    Create error to test linters.yml

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Calculator from './components/calculator';
 
 export default class App extends Component {
+  
+  
   render() {
     return (
       <Calculator />
