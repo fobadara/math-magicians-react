2bcb5e75c9b8bc9f82bff9ad6aa3149fdabaa4a0
Abdulfatai <fob90s@gmail.com>
2022-01-04 09:58:03 +0100

    Remove intentional error

--- src/app.js
+++ src/app.js
@@ -3,8 +3,6 @@ import React, { Component } from 'react';
 import Calculator from './components/calculator';
 
 export default class App extends Component {
-  
-  
   render() {
     return (
       <Calculator />
